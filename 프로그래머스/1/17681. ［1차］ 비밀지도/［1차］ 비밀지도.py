3ff7f89b66b2d1ddcbb6e952914fa69bd932000a
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2024-08-08 10:03:11 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,23 +2,14 @@ def solution(n, arr1, arr2):
     answer = []
     SPACE = " "
     WALL = "#"
+    
     for row_idx in range(n):
-        decoded_row_arr1 = list(bin(arr1[row_idx])[2:])
-        decoded_row_arr2 = list(bin(arr2[row_idx])[2:])
-        if len(decoded_row_arr1) < n:
-            len_diff = n - len(decoded_row_arr1)
-            decoded_row_arr1 = [0] * len_diff + decoded_row_arr1
+        # 비트 OR 연산 후 이진수 문자열로 변환
+        combined_row = bin(arr1[row_idx] | arr2[row_idx])[2:].rjust(n, '0')
         
-        if len(decoded_row_arr2) < n:
-            len_diff = n - len(decoded_row_arr2)
-            decoded_row_arr2 = [0] * len_diff + decoded_row_arr2
+        # 리스트 컴프리헨션을 사용하여 행 생성
+        row = [WALL if bit == "1" else SPACE for bit in combined_row]
         
-        row = []
-        for col_idx in range(n):
-            if decoded_row_arr1[col_idx] == "1" or decoded_row_arr2[col_idx] == "1":
-                row.append(WALL)
-                continue
-            
-            row.append(SPACE)
         answer.append(''.join(row))
+    
     return answer
