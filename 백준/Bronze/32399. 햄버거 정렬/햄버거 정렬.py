e20df8e9c00e4c0bce10160f7959ed8535ff6770
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2024-10-27 13:44:24 +0900

    [Bronze III] Title: 햄버거 정렬, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/32399. 햄버거 정렬/햄버거 정렬.py
@@ -0,0 +1,15 @@
+
+input_str = input()
+
+if input_str == "1)(":
+    print(1)
+elif input_str == ")1(":
+    print(2)
+elif input_str == "(1)":
+    print(0)
+elif input_str == ")(1":
+    print(1)
+elif input_str == "()1":
+    print(1)
+elif input_str == "1()":
+    print(1)
