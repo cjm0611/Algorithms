670bb2608ef3a41fe445fdc39701f3bdafcdd233
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2024-10-28 23:00:51 +0900

    [Silver III] Title: 게임, Time: 64 ms, Memory: 36528 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,13 +1,34 @@
+# import sys
+
+# readline = sys.stdin.readline
+
+# X, Y = map(int, readline().split())
+# if X == Y:
+#     print(-1)
+#     exit()
+
+# rate = int((Y*100)/X)
+
+# left = 1
+# right = X
+# is_change = False
+# answer = -1
+# while left <= right:
+#     mid = (left + right) // 2 # 게임을 더 하는 횟수
+#     tmp_X = X + mid
+#     tmp_Y = Y + mid
+#     tmp_rate = int((tmp_Y*100)/tmp_X)
+#     if tmp_rate > rate:
+#         answer = mid
+#         right = mid - 1
+#     else:
+#         left = mid + 1
+
+# print(answer)
+
+
 import sys
 from decimal import Decimal
-
-"""
-10 8
-1~10
-left:1, right:X
-mid = left + right // 2
-자기자신을 넘는 경우는 없음.
-"""
 
 readline = sys.stdin.readline
 
@@ -21,16 +42,22 @@ rate = int((Y*100)/X)
 left = 1
 right = X
 is_change = False
-answer = -1
 while left <= right:
     mid = (left + right) // 2 # 게임을 더 하는 횟수
     tmp_X = X + mid
     tmp_Y = Y + mid
     tmp_rate = int((tmp_Y*100)/tmp_X)
-    if tmp_rate > rate:
-        answer = mid
-        right = mid - 1
-    else:
+    if tmp_rate == rate:
         left = mid + 1
+    else:
+        is_change = True
+        right = mid - 1
 
-print(answer)
+if not is_change:
+    print(-1)
+else:
+    left_rate = int(((Y+left)*100)/(X+left))
+    if rate != left_rate:
+        print(left)
+    else:
+        print(right)
