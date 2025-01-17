f8235d0eb8da083bf71a8f097131003ef3f16202
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2025-01-17 11:57:10 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.py
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.py
@@ -11,7 +11,7 @@ N개의 수를 M개 이하의 구간으로 나눔.
 N, M = map(int, readline().split()) # N개의 수, M개 이하
 nums = list(map(int, readline().split()))
 
-def check(mid):
+def check(mid): # 구간이 M개 이하로 나눠지고, 각 구간의 차이값이 mid 이하인지
     cnt = 1
     min_v, max_v = float('inf'), -1
     for num in nums:
