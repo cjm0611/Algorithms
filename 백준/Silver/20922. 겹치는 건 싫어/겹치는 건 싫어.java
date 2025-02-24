c708f41e5644a1686cbeda78d54b28473a112c58
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2025-02-24 20:18:06 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 444 ms, Memory: 61360 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, K;
+    static int[] inputs;
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        inputs = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            inputs[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int l = 0, r = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while (l <= r && r < inputs.length) {
+            int rKey = inputs[r];
+            int rCnt = map.getOrDefault(rKey, 0);
+            if (rCnt < K) {
+                if (rCnt == 0) {
+                    map.put(rKey, 0);
+                }
+
+                map.replace(rKey, rCnt+1);
+                answer = Math.max(answer, r-l+1);
+                r++;
+                continue;
+            }
+
+            int lKey = inputs[l];
+            int lCnt = map.get(lKey);
+            map.replace(lKey, lCnt-1);
+            l++;
+        }
+
+        System.out.println(answer);
+    }
+}
